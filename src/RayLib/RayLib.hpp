1c19eba1ecd285f769822da3a1f3696bb263e5c9
Arthur Junges <arthur.junges@epitech.eu>
2021-05-14 10:30:05 +0200

    Adding Gtest cross platform for DynamicLibrary

--- src/RayLib/RayLib.hpp
+++ src/RayLib/RayLib.hpp
@@ -14,17 +14,9 @@
 
 #if defined _WIN32 || defined __CYGWIN__
 #ifdef BUILDING_DLL
-    #ifdef __GNUC__
-      #define DLL_PUBLIC __attribute__ ((dllexport))
-    #else
-      #define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
-    #endif
+    #define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
   #else
-    #ifdef __GNUC__
-      #define DLL_PUBLIC __attribute__ ((dllimport))
-    #else
-      #define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
-    #endif
+    #define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
   #endif
   #define DLL_LOCAL
 #else
