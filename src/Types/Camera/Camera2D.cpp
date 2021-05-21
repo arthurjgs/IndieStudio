283493e0223bd7415beda75f37f0ef84959eba9d
alexandre.burger@epitech.eu <alexandre.burger@epitech.eu>
2021-05-21 12:31:13 +0200

    ADD: :camera_flash:  type of camera2D

---
+++ src/Types/Camera/Camera2D.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file Camera2D.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "Camera2D.hpp"
+
+Type::Camera2D::Camera2D()
+{
+}
+
+Type::Camera2D::Camera2D(const Type::Vector<2> offset, const Type::Vector<2> target, const float rotation, const float zoom)
+{
+    _offset = offset;
+    _target = target;
+    _rotation = rotation;
+    _zoom = zoom;
+}
+
+Type::Camera2D::Camera2D(const Type::Camera2D &cpy)
+{
+    _offset = cpy._offset;
+    _target = cpy._target;
+    _rotation = cpy._rotation;
+    _zoom = cpy._zoom;
+}
+
+Type::Camera2D Type::Camera2D::operator =(const Camera2D &cpy)
+{
+    _offset = cpy._offset;
+    _target = cpy._target;
+    _rotation = cpy._rotation;
+    _zoom = cpy._zoom;
+    return (*this);
+}
+
+Type::Camera2D::~Camera2D()
+{
+}
