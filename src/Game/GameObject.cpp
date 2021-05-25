fd54795526b1224ba8bb6271ec64d7c17333083d
Arthur Junges <arthur.junges@epitech.eu>
2021-05-25 16:30:38 +0200

    Add GameObject items

---
+++ src/Game/GameObject.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2020
+** delivery
+** File description:
+** delivery
+*/
+
+#include "GameObject.hpp"
---
+++ src/Game/GameObject.hpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2020
+** delivery
+** File description:
+** delivery
+*/
+
+#ifndef BOMBERMAN_GAMEOBJECT_HPP
+#define BOMBERMAN_GAMEOBJECT_HPP
+
+#endif //BOMBERMAN_GAMEOBJECT_HPP
