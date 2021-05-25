5d73913ceb6a187c4b366a93b8ccd73a7f23a172
Arthur Junges <arthur.junges@epitech.eu>
2021-05-25 16:54:50 +0200

    [GAMEOBJECT] Added GameObject

--- src/Game/GameObject.hpp
+++ src/Game/GameObject.hpp
@@ -8,4 +8,48 @@
 #ifndef BOMBERMAN_GAMEOBJECT_HPP
 #define BOMBERMAN_GAMEOBJECT_HPP
 
+#include "Types/Vector/Vector.hpp"
+
+namespace Bomberman {
+    class GameObject {
+    public:
+        enum ObjectType {
+            PLAYER,
+            DECOR,
+            DESTRUCTIBLE,
+            BOMB,
+            BONUS,
+            BUTTON,
+            TEXT,
+            CAMERA,
+            MUSIC,
+            SOUND
+        };
+
+        GameObject() = delete;
+        GameObject(const GameObject &) = delete;
+        GameObject &operator=(const GameObject &) = delete;
+
+        GameObject(const std::string &name, ObjectType type, const Type::Vector<3> &position, bool display = true);
+        virtual ~GameObject();
+
+        void setPosition(Type::Vector<3> position);
+        Type::Vector<3> getPosition();
+
+        std::string getName() const;
+
+        ObjectType getType();
+
+        void setDisplay(bool display);
+        bool getDisplay();
+
+        virtual void render() = 0;
+    private:
+        Type::Vector<3> _position;
+        std::string _name;
+        ObjectType _type;
+        bool _display;
+    };
+}
+
 #endif //BOMBERMAN_GAMEOBJECT_HPP
